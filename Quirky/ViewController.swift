f616d859e3cfb50d0ac202534f427810609d0577
zmkhtr <azammukhtar3@gmail.com>
2020-06-08 19:36:58 +0700

    initial commit

---
+++ Quirky/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quirky
+//
+//  Created by Azam Mukhtar on 08/06/20.
+//  Copyright © 2020 Azam Mukhtar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
